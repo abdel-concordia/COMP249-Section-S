a37225b5844afc49a8bd5514c6d3f163735bc94d
Abdel MSSPRO <abdel@MSSPRO>
2025-01-22 09:28:44 -0500

    Example 1 with address as String

--- COMP249_Section_S/src/comp249_section_s/DriverClass.java
+++ COMP249_Section_S/src/comp249_section_s/DriverClass.java
@@ -4,6 +4,19 @@ public class DriverClass {
 
     public static void main(String[] args) {
 
+        // Use the parameterized constructor
+        Person person1 = new Person("John", 10, "Montreal");
+
+        // Using the default constructor
+        Person person2 = new Person();
+
+        // Using the copy constructor
+        Person person3 = new Person(person1);
+
+        boolean b = person1.equals(person2);
+
+        System.out.println(person1);
+
     }
 
 }
--- COMP249_Section_S/src/comp249_section_s/Person.java
+++ COMP249_Section_S/src/comp249_section_s/Person.java
@@ -19,10 +19,57 @@ public class Person {
     // Default constructor
     public Person() {
         this("Unknown name", 0, "Unknow address"); // calls another constructor
-
-        //this.name = "Unknown name";
-        //this.age = 0;
-        //this.address = "Unknown address";
     }
 
+    // Copy constructor (watch out!!)
+    public Person(Person otherPerson) {
+        this(otherPerson.name, otherPerson.age, otherPerson.address);
+    }
+
+    // Getters
+    public String getName() {
+        return this.name; // privacy leak??
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean setAge(int age) {
+        if (age >= 0) {
+            this.age = age;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean equals(Person otherPerson) { // Will be improved later
+        if (otherPerson == null) {
+            return false;
+        }
+
+        if (this.name.equals(otherPerson.name) && this.age == otherPerson.age) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "This is " + this.name + " whose age is " + this.age;
+    }
 }
