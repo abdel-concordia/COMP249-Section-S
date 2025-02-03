15e948b362f0f2126f480651d4ab1e38e938aae4
Abdel MSSPRO <abdel@MSSPRO>
2025-02-03 09:53:50 -0500

    Example of inheritance with static data attribute

--- COMP249_Section_S/src/comp249_section_s/DriverClass.java
+++ COMP249_Section_S/src/comp249_section_s/DriverClass.java
@@ -5,8 +5,29 @@ public class DriverClass {
     public static void main(String[] args) {
 
         //Address address1 = new Address(1445, "Maisonneuve West", "Montreal");
-        Student st1 = new Student("John", 10, new Address(1445, "Maisonneuve West", "Montreal"), "112233", 0.0);
+        //Student st1 = new Student("John", 10, new Address(1445, "Maisonneuve West", "Montreal"), "112233", 0.0);
+        A a = new A();
+        B b = new B();
+
+        System.out.println(a.x);
+        System.out.println(b.x);
 
     }
 
+}
+
+class A {
+
+    static int x = 0;
+
+    public A() {
+        x++;
+    }
+}
+
+class B extends A {
+
+    public B() {
+        x++;
+    }
 }
