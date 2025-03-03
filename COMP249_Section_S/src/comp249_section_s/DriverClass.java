b8334b3b5fc59ae85649051c0f588d3c960c01ac
Abdel MSSPRO <abdel@MSSPRO>
2025-03-03 09:37:44 -0500

    Writing to a binary file

--- COMP249_Section_S/src/comp249_section_s/DriverClass.java
+++ COMP249_Section_S/src/comp249_section_s/DriverClass.java
@@ -1,49 +1,44 @@
 package comp249_section_s;
 
-import java.io.PrintWriter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
-
-import java.util.Scanner;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class DriverClass {
 
     public static void main(String[] args) {
 
-        // Writing to a text file
-        // Create PrintWriter object
-        PrintWriter fos = null;
+        int x = 10, y = 7, z = 25;
+
+        // Open/create the file
+        ObjectOutputStream oos = null;
         try {
-            fos = new PrintWriter(new FileOutputStream("data.txt"));
+            oos = new ObjectOutputStream(new FileOutputStream("data.bin"));
         } catch (FileNotFoundException fnfex) {
-            System.out.println("Cannot create the file for output.");
+            System.out.println("Cannot create/open the file.");
+            System.exit(0);
+        } catch (IOException ioex) {
+            System.out.println("Cannot write to the file.");
+            System.exit(0);
+        }
+        // Write from/to file
+        try {
+            oos.writeInt(x);
+            oos.writeInt(y);
+            oos.writeInt(z);
+        } catch (IOException ioex) {
+            System.out.println("Cannot write to the file.");
             System.exit(0);
         }
 
-        // Write to the file
-        fos.print("Hello");
-        fos.println(" World");
-        fos.print("Hi");
-
-        // Close the file
-        fos.close();
-
-        // Reading from a text file
-        // Open the file
-        Scanner sc = null;
+// close the file
         try {
-            sc = new Scanner(new FileInputStream("data.txt"));
-        } catch (FileNotFoundException fnfex) {
-            System.out.println("Could not open the file for input.");
+            oos.close();
+        } catch (IOException ioex) {
+            System.out.println("Cannot close the output file.");
+            System.exit(0);
         }
 
-        // Read from file
-        while (sc.hasNextLine()) { // returns true as long as there is a line to read
-            String s = sc.nextLine();
-            System.out.println(s);
-        }
-        // Close file
-        sc.close();
     }
 }
