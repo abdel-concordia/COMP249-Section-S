6688e55bb39511a069bc0d27e71226d32dfeac3c
Abdel MSSPRO <abdel@MSSPRO>
2025-03-19 09:38:10 -0400

    ArrayList example 2 with Comparable

--- COMP249_Section_S/src/comp249_section_s/DriverClass.java
+++ COMP249_Section_S/src/comp249_section_s/DriverClass.java
@@ -1,6 +1,8 @@
 package comp249_section_s;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class DriverClass {
 
@@ -9,9 +11,47 @@ public class DriverClass {
         ArrayList<String> myStrings = new ArrayList<>(10); // Initial capacity, NOT limit
 
         myStrings.add("Hello");
-        myStrings.add("hello again");
+        myStrings.add("Bye");
 
-        ArrayList<Integer> myNumbers = new ArrayList<>();
+        System.out.println("Before sort: ");
+        for (String s : myStrings) {
+            System.out.println(s);
+        }
+        Collections.sort(myStrings);
+
+        System.out.println("After sort: ");
+
+        for (String s : myStrings) {
+            System.out.println(s);
+        }
+
+        ArrayList<Course> objs = new ArrayList<>(5);
+
+        Collections.sort(objs); // Looks for compareTo that takes Object
+
+        Course c1 = new Course();
+
+    }
+
+}
+
+class Course implements Comparable {
+
+    private String code;
+    private String title;
+    private double creditPoints;
+
+    @Override
+    public int compareTo(Object otherObject) {
+        // Make sure you can cast
+        Course otherCourse = (Course) otherObject;
+        if (this.creditPoints < otherCourse.creditPoints) {
+            return -1;
+        } else if (this.creditPoints > otherCourse.creditPoints) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
 }
