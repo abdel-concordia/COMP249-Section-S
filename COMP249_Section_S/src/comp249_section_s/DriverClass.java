4af0f07404d0354cfec3b3df38c1e2adb972309b
Abdel MSSPRO <abdel@MSSPRO>
2025-04-07 10:11:32 -0400

    Comparator example

--- COMP249_Section_S/src/comp249_section_s/DriverClass.java
+++ COMP249_Section_S/src/comp249_section_s/DriverClass.java
@@ -1,19 +1,70 @@
 package comp249_section_s;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class DriverClass {
 
     public static void main(String[] args) {
 
-        CLinkedList<String> list1 = new CLinkedList<>();
+        ArrayList<A> arrayOfA = new ArrayList<>();
 
-        list1.addToHead("10");
-        //list1.addToHead("5");
-        //list1.addToHead("4");
-        //list1.addToHead("3");
+        Collections.sort(arrayOfA);
 
-        list1.display();
+        Collections.sort(arrayOfA, new ComparatorBasedOnX()); // method1
+
+        Collections.sort(arrayOfA, new ComparatorBasedOnY()); // method2 in class B
+
     }
 
+}
+
+class A implements Comparable {
+
+    private int x;
+    private int y;
+
+    public A(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override  // List<List<string>>
+    public int compareTo(Object otherObject) {
+        A otherA = (A) otherObject;
+        return Integer.compare(this.x, otherA.x);
+    }
+}
+
+class ComparatorBasedOnX implements Comparator<A> {
+
+    @Override
+    public int compare(A a1, A a2) {
+        return Integer.compare(a1.getX(), a2.getX());
+    }
+}
+
+class ComparatorBasedOnY implements Comparator<A> {
+
+    @Override
+    public int compare(A a1, A a2) {
+        return Integer.compare(a1.getY(), a2.getY());
+    }
 }
